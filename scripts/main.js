a8fe818b409f6ddc7cda295b1e28dfbe48ba8b96
Kevin Springer <KevinUSCU@outlook.com>
2017-11-09 11:25:45 -0800

    Add base style and script.

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+// Navbar Listeners
+$('#index-button')[0].addEventListener('click', event => window.location="film-index.html")
+$('#add-button')[0].addEventListener('click', event => window.location="add-film.html")
