b89d4c89177cd9d682a3a33fd775cad910fbad10
Kevin Springer <KevinUSCU@outlook.com>
2017-11-09 15:09:50 -0800

    Update link addresses.

--- scripts/display-index.js
+++ scripts/display-index.js
@@ -19,7 +19,7 @@ function buildItem(movie) {
   let star = '⭐️ '
   let indexRow = `
     <tr id=${id}>
-      <td><a href="film.html?film=${id}">${title}</a></td>
+      <td><a href="view-film.html#/movie/${id}">${title}</a></td>
       <td>${director}</td>
       <td>${year}</td>
       <td>${star.repeat(star_rating)}</td>
@@ -37,7 +37,7 @@ function listen() {
       // Get ID of movie
       const id = clicked.parent().parent()[0].id
       if (clicked.hasClass('index-delete-button')) deleteMovie(id).then(() => displayIndex())
-      else if (clicked.hasClass('index-edit-button')) window.location=`edit-film?film=${id}`
+      else if (clicked.hasClass('index-edit-button')) window.location=`edit-film.html#/movie/${id}`
     }
   })
 }
