5ca13f06502714cf39035e015f5a385bce79d4ae
Kevin Springer <KevinUSCU@outlook.com>
2017-11-09 15:30:37 -0800

    Change film references to "movie".

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,5 @@
 const baseURL = 'http://localhost:3000'
 
 // Navbar Listeners
-$('#nav-index-button')[0].addEventListener('click', event => window.location="film-index.html")
-$('#nav-add-button')[0].addEventListener('click', event => window.location="add-film.html")
+$('#nav-index-button')[0].addEventListener('click', event => window.location="movie-index.html")
+$('#nav-add-button')[0].addEventListener('click', event => window.location="add-movie.html")
