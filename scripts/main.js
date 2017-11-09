9c30d9002babe0d2a45e4a8749621342f08da145
Kevin Springer <KevinUSCU@outlook.com>
2017-11-09 12:25:24 -0800

    Add baseURL for server.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+// Following line is to connect to deployed Heroku server
+// const baseURL = 'https://kevinuscu-movie-crud-server.herokuapp.com'
+// Following line is to connect to locally hosted server
+const baseURL = 'http://localhost:3000'
+
 // Navbar Listeners
 $('#nav-index-button')[0].addEventListener('click', event => window.location="film-index.html")
 $('#nav-add-button')[0].addEventListener('click', event => window.location="add-film.html")
