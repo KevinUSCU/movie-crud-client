2a9f11c208bee83d320bd4154eccf586f76347bd
Kevin Springer <KevinUSCU@outlook.com>
2017-11-09 23:12:59 -0800

    Swap to deployed baseURL.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Following line is to connect to deployed Heroku server
-// const baseURL = 'https://kevinuscu-movie-crud-server.herokuapp.com'
+const baseURL = 'https://kevinuscu-movie-crud-server.herokuapp.com'
 // Following line is to connect to locally hosted server
-const baseURL = 'http://localhost:3000'
+// const baseURL = 'http://localhost:3000'
 
 // Navbar Listeners
 $('#nav-index-button')[0].addEventListener('click', event => window.location="movie-index.html")
